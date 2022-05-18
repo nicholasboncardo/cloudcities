9990cad57098a1bd31aaeeea8594b4511cdfd82e
Lukas Frenzer <lukas.frenzer@web.de>
2022-05-18 22:44:58 +0200

    resize error

--- src/components/Testp5.svelte
+++ src/components/Testp5.svelte
@@ -57,9 +57,9 @@
 				} else {
 					canvas = p5.createCanvas(p5.windowWidth, canvasHeight);
 				}
-			} else if (p5.windowWidth <= 500 && image.width < image.height) {
+			} /*else if (p5.windowWidth < 500 && image.width < image.height) {
 				canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
-			} else if (p5.windowWidth <= 500) {
+			} */ else if (p5.windowWidth < 500) {
 				canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
 			}
 			p5.background(image);
@@ -139,9 +139,9 @@
 				}
 			} else if (p5.windowHeight < 500) {
 				if (canvasHeight < p5.windowHeight) {
-					canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+					canvas = p5.resize(p5.windowWidth, p5.windowHeight);
 				} else {
-					canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+					canvas = p5.resize(p5.windowWidth, p5.windowHeight);
 				}
 			} else if (p5.windowWidth < 500) {
 				p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
